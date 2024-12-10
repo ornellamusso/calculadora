a12e5fc602c12c1075008df167a558cabdff3027
ornella <ornellamussog@gmail.com>
2024-12-10 18:28:23 +0100

    Initial Commit

---
+++ calculadoraTests/calculadoraTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculadoraTests.swift
+//  calculadoraTests
+//
+//  Created by Tardes on 10/12/24.
+//
+
+import Testing
+@testable import calculadora
+
+struct calculadoraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
